cf6d9606effebb29a2bd84a81a7e529411b5c803
SolAZDev <SolAZDev@gmail.com>
2021-06-17 16:33:46 -0400

    PVue3

---
+++ src/views/Games.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.container
+  .p-mx-2.p-mx-md-3
+    .p-text-center
+        h3 Visual Works Portfolio
+        h5 Levels Designed & 3D Renders and Game Models
+    .p-grid
+          .p-md-6.p-lg-4(v-for="game in Games" :key="game.id") 
+            ThumbnailCard(:name="game.name" :link="'/games/'+game.id" :image="game.thumb")
+</template>
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import ThumbnailCard from '../components/ThumbnailCard.vue';
+import * as GameData from '../data/games';
+@Options({
+  components:{ThumbnailCard}
+})
+export default class Games extends Vue{
+    Games=GameData.default.Games
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,25 @@
 <template lang='pug'>
 .p-mx-2.p-mx-lg-6
-  .p-text-center
-    h2 SolAZDev
-    h5 Indie Game & Web Developer
-  .p-grid(v-if="newStyle")
-    .p-lg-6
-      .embed-responsive.embed-responsive-16by9
-        iframe(:src="Data.reel", allow='autoplay; encrypted-media; picture-in-picture', allowfullscreen).embed-responsive-item
-    .p-lg-6
-      h3.p-text-center About
-      p.p-text-justify {{about[0]}}
-      p.p-text-justify {{about[0]}}
-      br
-      h3.p-text-center Connect
-      .p-grid.iconList
-        .p-col #[IconLink(link="http://github.com/SolAZDev", :icon="['fab','github']" size="2x")]
-        .p-col #[IconLink(link="https://solazdev.itch.io/", :icon="['fab','itch-io']" size="2x")]
-        .p-col #[IconLink(link="https://twitter.com/SolAZDev", :icon="['fab','twitter']" size="2x")]
-        .p-col #[IconLink(link="https://www.linkedin.com/in/solazdev/", :icon="['fab','linkedin']" size="2x")]
-        .p-col #[IconLink(:link="'mailto:'+Data.email", :icon="['fas','at']" size="2x")]
-  //- <div class="home">
-  //-   <img alt="Vue logo" src="../assets/logo.png">
-  //-   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  //- </div>
+  .p-fluid
+    .p-text-center
+      h2 SolAZDev
+      h5 Indie Game & Web Developer
+    .p-grid(v-if="newStyle")
+      .p-lg-8
+        .embed-responsive.embed-responsive-16by9
+          iframe(:src="Data.reel", allow='autoplay; encrypted-media; picture-in-picture', allowfullscreen).embed-responsive-item
+      .p-lg-4
+        h3.p-text-center About
+        p.p-text-justify.about {{about[0]}}
+        p.p-text-justify.about {{about[0]}}
+        br
+        h3.p-text-center Connect
+        .p-grid.iconList
+          .p-col #[IconLink(link="http://github.com/SolAZDev", :icon="['fab','github']" size="2x")]
+          .p-col #[IconLink(link="https://solazdev.itch.io/", :icon="['fab','itch-io']" size="2x")]
+          .p-col #[IconLink(link="https://twitter.com/SolAZDev", :icon="['fab','twitter']" size="2x")]
+          .p-col #[IconLink(link="https://www.linkedin.com/in/solazdev/", :icon="['fab','linkedin']" size="2x")]
+          .p-col #[IconLink(:link="'mailto:'+Data.email", :icon="['fas','at']" size="2x")]
 </template>
 
 <script lang="ts">
@@ -39,7 +36,7 @@ export default class Home extends Vue {
   newStyle=true;
   Data = info.default;
   about = [
-    "Hello! I'm Carlos E. Orama, pen name; SolAZDev. I'm an Indie Game Developer and a Web Designer, with a passion for learning new technologies. I've specialized in, but not limited to, Gameplay Programming, Level Design for Games, Front End, Responsive Design, Server Communications for Web",
+    "Hello! I'm Carlos  E. Orama, pen name; SolAZDev. I'm an Indie Game Developer and a Web Designer, with a passion for learning new technologies. I've specialized in, but not limited to, Gameplay Programming, Level Design for Games, Front End, Responsive Design, Server Communications for Web",
     "My tools of choice include, but are not limited to several proven tools & frameworks such as; Blender, Unity3D, NodeJS, Angular, VueJS and more. You can see some of my works in the Games and Other Works section, as well as some of visual work on the Portfolio section"
     
   ]
@@ -49,6 +46,9 @@ export default class Home extends Vue {
 @import "../assets/theme.scss";
 h2{margin-bottom: 0;}
 h5{margin-top: 0;}
+.about{
+  font-size: 1.25rem;
+}
 .iconList{
   a{
     color: $textSecondaryColor;
