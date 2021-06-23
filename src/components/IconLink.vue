0cb28f9a3f52134d5c98fba632ead44f94367f1a
SolAZDev <SolAZDev@gmail.com>
2021-06-23 15:05:54 -0400

    I should've went with Quasar from a start.

---
+++ src/components/IconLink.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+a.text-primary(:href="link", target="_blank")
+  q-icon(:name="icon", :size="size")
+  div(v-if="name!=''") {{ name }}
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import { Component } from 'vue-property-decorator';
+
+@Options({
+  props: {
+    link: String,
+    icon: String,
+    size: String,
+    name: String
+  }
+})
+export default class IconLink extends Vue {
+  link!: string
+  icon!:string
+  size!:string
+  name=""
+}
+</script>
+
+<style lang="sass" scoped></style>
---
+++ src/components/ThumbnailCard.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+router-link(:to="link")
+  q-img(:src='publicPath+image', :ratio='1', spinner-color='primary', spinner-size='82px')
+    .absolute-bottom.text-subtitle1.text-center(v-if="name!=''") {{name}}
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+    name: String,
+    link: String,
+    image: String,
+    ar: String
+  },
+})
+export default class ThumbnailCard extends Vue {
+  name!: string
+  icon!:string
+  image!:string
+  ar="4/3"
+  // publicPath= process.env.BASE_URL
+  publicPath= "/media/"
+}
+</script>
