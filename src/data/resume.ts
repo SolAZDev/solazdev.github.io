aaf5972c4e3bc4021e090393161c97abc1d7cb02
SolAZDev <SolAZDev@gmail.com>
2022-03-12 22:18:10 -0400

    Resume Spring Cleaning Pt1

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -3,16 +3,16 @@ export default {
   name: "Carlos E. Orama De Jesus",
   subtitle: "Game & Web Developer",
   local: "Gurabo, Puerto Rico",
-  email: "SolAZDev@gmail.com",
+  email: "SolAZDev@gmail.com", number: "939-244-2231",
   objective: "As a Developer & Designer, I am looking for a position where I can grow, and use the best of my abilities to push projects forward, and surpass if not meet the goals and expectations, efficiently and effectively. Creating or using modular and reusable bases to facilitate development, be it for Games, Software, 3D Models or Web. I hope to find opportunities that may aid in mutual professional growth.",
   v: ["Over 3 years of experience in game development, and 3 years of web development", "Proficient "],
   i: ["Implemented Behavior Trees and Ut  ility-based AI systems in Unity and C#", "Rapid Efficient Development & Prototyping and testing for multiple systems.",],
   c: ["Lead the development of at least 3 games, some of which have been released.", "Regular back-and-forth conversations to find the most efficient way to solve problems."],
   skills: {
     major: ["Strong Attention to Detail", "3D Modeling & 3D 3D Printing", "Modular Set & Level Design", "Video Editing", "Key-frame Animation", "Fully Bilingual; English & Spanish",],
-    minor: ["UX & UI Design", "Code Documentation with Doxygen", "Story Writing", "Storyboarding", "Multi-phase Planning", "Agile", "Scrum", "Bug Reporting"],
-    frameworks: ["C#", "Javascript", "TypeScript", "Pug/Jade", "Linux", "Bash", "FiSH", "Vue", "Quasar", "Angular", "Node", "Entity Framework"],
-    software: ["Unity", "VSCode", "Blender", "Material Maker", "Photoshop", "Illustrator", "Inkscape", "Krita", "GIMP", "Audacity", "Game Maker", "Doxygen", "Git", "JIRA", "GitHub"]
+    minor: ["UX & UI Design", "Code Documentation with Doxygen", "Story Writing", "Storyboarding", "Multi-phase Planning", "Agile", "Scrum", "Bug Reporting", "Continuous Integration"],
+    frameworks: ["C#", "Javascript", "TypeScript","Vue", "Quasar Framework", "Angular", "Node", "Pug/Jade", "Linux", "Bash", "FiSH",  "Entity Framework"],
+    software: ["Unity", "VS Code", "Blender", "Material Maker", "Photoshop", "Illustrator", "Inkscape", "Krita", "GIMP", "Audacity", "Game Maker", "Doxygen", "Git", "JIRA", "GitHub"]
   },
   education: [{ locale: " Atlantic University College", years: "2011-2015", degree: "BA. Science of Video Game Design" }],
   awards: [{ name: "Eagle Scout Rank", by: "Boy Scouts of America (T432 PR)" }, { name: "Best Use of Themes", by: "Puerto Rico Global Game Jam 2018" }],
@@ -20,79 +20,67 @@ export default {
     {
       position: "Software Developer",
       employer: "Smartlogix Innovation Labs",
-      duration: "May 2020 - August 2021",
       responsibilities: [
-        "Developed various and pages and web components, and their backend functions for the SmartCare line of applications.",
-        "Created an efficient workflow standard for web development for the Operator's Management Portal application",
-        "Created a complete, web-based, cross-platform, Remote Desktop Solution for technical support.",
-        "Identified shortcomings and oversights, and developed solutions to circumvent said issues based on user and team feedback.",
+        "Developed various and pages and web components, and their backend functionality for the SmartCare line of products.",
+        "Established and Standardized an efficient web development workflow for multiple projects.",
+        "Created a web-based, Remote Desktop Solution for technical support.",
+        "Identified shortcomings and oversights, and developed solutions to circumvent them based on user and team feedback.",
         "Extensively test multiple scenarios to ensure reliability across the suite of applications."
       ]
     },
     {
       position: "Jr. Software Developer",
       employer: "PRSoft, Inc",
-      duration: "Oct 2019 - Feb 2020",
       responsibilities: [
-        "Developed various new pages of the web app, PRCorp. Which was Angular Based",
-        "Responsible with translating segments in the backend, from C# to F#.",
-        "Developed new segments of a backend server in F# and PostgreSQL.",
-        "Extensively tested software to ensure there were no downtimes, cyclic dependencies, or UI and UX inconsistencies."
+        "Developed various new pages and segments of Angular based, PRCorp web app.",
+        "Efficiently translated parts of the backend, from C# to F#, & developed new segments in F#, which included communicating with a PostgreSQL database.",
+        "Extensively tested software to ensure there were no downtimes, cyclic dependencies, or UI/UX inconsistencies."
       ]
     },
     {
       position: "Full Stack Developer",
       employer: "IfIdea, LLC",
-      duration: "Nov 2018 - May 2019",
       responsibilities: [
-        "Developed an ecosystem for a client company.",
-        "Developed the Administration web app and Employee mobile app, both with Angular 6 and Ionic.",
-        "Designed a MariaDB Database and developed the communications server in PHP with the Slim framework.",
-        "Streamlined the User Interface for both applications based on feedback ."
+        "In charge of developing a Full Stack Ecosystem for clients",
+        "Developed Administration & Employee applications with Angular & Ionic",
+        "Developed a MariaDB Database and PHP Server using Slim Framework for communications.",
+        "Streamlined User Interfaces for both applications based on user feedback ."
       ]
     }, {
       position: "Lead Game Developer",
       employer: "Require Technology PR",
-      duration: "Feb 2018 - Sept 2018",
       responsibilities: [
-        "In charge of leading the development, and testing of Genial Skills Web.",
+        "In charge of Development and Quality Assurance of Genial Skills Web.",
+        "Developed various mini-games for multiple types of quizzes, an Avatar System ",
         "Development of the game was done with Unity3D Game Engine.",
-        "Developed various mini-games for multiple types of quizzes.",
-        "Developed an Avatar System, and an In App Purchase system.",
-        "Playtested and documented bugs, design issues, and oversights, and made updated to circumvent them, based on team feedback.",
-        "Documented the entirety of the software's code using Doxygen.",
+        "Developed the Avatar System and Integrated In App Purhcases",
+        "Tested and Documented Bugs, Design Issues and Oversights, and implemented ways to circumvent with aid of feedback",
+        "Extensively Documented the Project with Doxygen",
         "With my contributions, the product is now on the market."
       ]
     },
     {
       position: "Enviroment Designer & VR Interactive Programmer",
       employer: "SI3D Media",
-      duration: "Dec 2016 - Feb 2017",
       responsibilities: [
+        "In charge of Design and Development for multiple VR Projects.",
         "Created VR commercials for clients",
         "Developed various promotional mini-games for VR",
-        "Modeled various assets for use in production.",
-        "Playtested each project, documenting bugs, inconsistencies, and possible moments"
+        "Created various assets the were used in production.",
+        "Extensively tested ech project, Documenting bugs & oversights, inconsistencies, and implemented ways to circumvent them."
       ]
     },
     {
       position: "Game Design Instructor",
-      employer: "Esc. Mariano Abril Middle School",
-      duration: "Feb 2016 - May 2016",
+      employer: "Mariano Abril Jr. High",
       responsibilities: [
+        "In charge of teaching and mentoring groups of students.",
         "Taught the principles of 3D Modeling and Animation, using Blender.",
-        "Taught the principles of programming in C#",
+        "Taught the principles of programming in C# and JavaScript.",
         "Taught how to develop games in Unity.",
         "Mentored students regarding their creative process."
       ]
     },
-    // {
-    //   position: "",
-    //   employer: "",
-    //   duration: "",
-    //   responsibilities: [
-    //     ""
-    //   ]
-    // }
+
   ]
 }
