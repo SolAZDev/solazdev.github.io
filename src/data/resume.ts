dae1fe1d1fcb035cf51f54b06b7df4bc1e9df783
SolAZDev <SolAZDev@gmail.com>
2022-03-23 01:52:06 -0400

    Resume update WIP

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -1,13 +1,12 @@
 export default {
   icon: "/media/solAvatar.svg",
   name: "Carlos E. Orama De Jesus",
-  subtitle: "Game & Web Developer",
-  local: "Gurabo, Puerto Rico",
+  subtitle: "Game, Web & Software Developer",
   email: "SolAZDev@gmail.com", number: "939-244-2231",
   objective: "As a Developer & Designer, I am looking for a position where I can grow, and use the best of my abilities to push projects forward, and surpass if not meet the goals and expectations, efficiently and effectively. Creating or using modular and reusable bases to facilitate development, be it for Games, Software, 3D Models or Web. I hope to find opportunities that may aid in mutual professional growth.",
   v: ["Over 3 years of experience in game development, and 3 years of web development", "Proficient "],
-  i: ["Implemented Behavior Trees and Ut  ility-based AI systems in Unity and C#", "Rapid Efficient Development & Prototyping and testing for multiple systems.",],
-  c: ["Lead the development of at least 3 games, some of which have been released.", "Regular back-and-forth conversations to find the most efficient way to solve problems."],
+  i: ["Implemented Behavior Trees and Utility-based AI systems in Unity and C#", "Rapid Efficient Development & Prototyping and testing for multiple systems.",],
+  c: ["Lead the development of at least 3 games, some of which have been released.", "Regular and effective back-and-forth conversations to find the most efficient way to solve problems."],
   skills: {
     major: ["Strong Attention to Detail", "3D Modeling & 3D 3D Printing", "Modular Set & Level Design", "Video Editing", "Key-frame Animation", "Fully Bilingual; English & Spanish",],
     minor: ["UX & UI Design", "Code Documentation with Doxygen", "Story Writing", "Storyboarding", "Multi-phase Planning", "Agile", "Scrum", "Bug Reporting", "Continuous Integration"],
@@ -15,17 +14,26 @@ export default {
     software: ["Unity", "VS Code", "Blender", "Material Maker", "Photoshop", "Illustrator", "Inkscape", "Krita", "GIMP", "Audacity", "Game Maker", "Doxygen", "Git", "JIRA", "GitHub"]
   },
   education: [{ locale: " Atlantic University College", years: "2011-2015", degree: "BA. Science of Video Game Design" }],
-  awards: [{ name: "Eagle Scout Rank", by: "Boy Scouts of America (T432 PR)" }, { name: "Best Use of Themes", by: "Puerto Rico Global Game Jam 2018" }],
+  awards: [{ name: "Eagle Scout Rank", by: "Boy Scouts of America (T432 PR)" }, { name: "Best Use of Theme", by: "Puerto Rico Global Game Jam 2018" }],
   work: [
+    {
+      position: "General Programmer",
+      employer: "Gladius Studios",
+      responsibilities:[
+        "Recreated the website using modern web development technologies.",
+        "Developed various Apps and Experiences for Client, using web app and game engine development suites.",
+        "Established and Standardized a defacto, efficient and rapid development workflow for app development.",
+        "Continuously collaborated with coworkers from multiple departments to ensure smooth production and innovative techniques to further improve projects."
+      ]
+    },
     {
       position: "Software Developer",
       employer: "Smartlogix Innovation Labs",
       responsibilities: [
-        "Developed various and pages and web components, and their backend functionality for the SmartCare line of products.",
-        "Established and Standardized an efficient web development workflow for multiple projects.",
+        "Developed various and pages and web components, and their backend functionality for the SmartCare line of products, and extensively test multiple scenarios to ensure reliability across the ecosystem.",
         "Created a web-based, Remote Desktop Solution for technical support.",
+        "Established and Standardized an efficient web development workflow for multiple projects.",
         "Identified shortcomings and oversights, and developed solutions to circumvent them based on user and team feedback.",
-        "Extensively test multiple scenarios to ensure reliability across the suite of applications."
       ]
     },
     {
@@ -41,22 +49,18 @@ export default {
       position: "Full Stack Developer",
       employer: "IfIdea, LLC",
       responsibilities: [
-        "In charge of developing a Full Stack Ecosystem for clients",
-        "Developed Administration & Employee applications with Angular & Ionic",
-        "Developed a MariaDB Database and PHP Server using Slim Framework for communications.",
+        "In charge of developing a Full Stack Ecosystem for clients. Which included Administrative and Employee apps",
+        "Apps were developed with Angular & Ionic, while the database was made with MariaDB, along with a PHP Server using Slim Framework for communications.",
         "Streamlined User Interfaces for both applications based on user feedback ."
       ]
     }, {
       position: "Lead Game Developer",
       employer: "Require Technology PR",
       responsibilities: [
-        "In charge of Development and Quality Assurance of Genial Skills Web.",
-        "Developed various mini-games for multiple types of quizzes, an Avatar System ",
-        "Development of the game was done with Unity3D Game Engine.",
-        "Developed the Avatar System and Integrated In App Purhcases",
+        "In charge of Development and Quality Assurance, and Documentation of Genial Skills Web, created with the Unity Game Engine.",
+        "Developed various mini-games for multiple types of quizzes, an Avatar System, and Integrated an In-App Purchase system.",
         "Tested and Documented Bugs, Design Issues and Oversights, and implemented ways to circumvent with aid of feedback",
-        "Extensively Documented the Project with Doxygen",
-        "With my contributions, the product is now on the market."
+        "With my contributions, the product has now been on the market."
       ]
     },
     {
@@ -64,9 +68,7 @@ export default {
       employer: "SI3D Media",
       responsibilities: [
         "In charge of Design and Development for multiple VR Projects.",
-        "Created VR commercials for clients",
-        "Developed various promotional mini-games for VR",
-        "Created various assets the were used in production.",
+        "Created VR commercials and promotional mini-games for clients. As well as assets that were used in production.",
         "Extensively tested ech project, Documenting bugs & oversights, inconsistencies, and implemented ways to circumvent them."
       ]
     },
@@ -74,11 +76,9 @@ export default {
       position: "Game Design Instructor",
       employer: "Mariano Abril Jr. High",
       responsibilities: [
-        "In charge of teaching and mentoring groups of students.",
-        "Taught the principles of 3D Modeling and Animation, using Blender.",
-        "Taught the principles of programming in C# and JavaScript.",
-        "Taught how to develop games in Unity.",
-        "Mentored students regarding their creative process."
+        "In charge of teaching and mentoring groups of students regarding their creative process.",
+        "Taught the principles of 3D Modeling and Animation, using Blender. And the principles of programming in C# & JavaScript.",
+        "Taught how to develop games in the Unity Game Engine.",
       ]
     },
 
--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -1,66 +1,79 @@
 <template lang="pug">
-q-page.q-px-md
-  //- .gt-lg.q-pb-lg.print-hide.text-caption.text-left When printing, please select paper: Legal (US), in Landscape, No Margins and 95% Scale
+q-page.q-px-xl
+  //- .gt-lg.q-pb-lg.print-hide.text-caption.text-left When printing, please select paper: Legal (US), in Landscape, No Margins and 99% Scale
   .row.q-col-gutter-md.resume
     .col-12.col-md-4.col-lg-3.rc
       .row.q-col-gutter-sm
-        .col-12.row.justify-center
-          q-img(:src='Resume.icon', :ratio='1', spinner-color='primary', spinner-size='82px', width="35%")
-        .col-12.text-center
-          .text-h6.text-primary.text-center {{Resume.name}}
-          .text-subtitle2.text-primary.text-center {{Resume.subtitle}}
-          .text-subtitle2 {{Resume.email}} - {{Resume.local}}
+        .col-12.text-center.q-mt-lg
+          .text-h4.text-primary.text-center {{Resume.name}}
+          .text-h6.text-primary.text-center {{Resume.subtitle}}
+          .text-body1 {{Resume.email}} - {{Resume.number}}
+
       q-space
-      .lt-md
+      .lt-md.q-my-xl
         .text-subtitle1.text-primary.smCmdL Current Work Objective
         .text-subtitle2.text-justify {{Resume.objective}}
-      div
-        .text-subtitle1.text-primary.smCmdL Major Skills
-        .text-subtitle2 {{listToText(Resume.skills.major)}}
-      div
-        .text-subtitle1.text-primary.smCmdL Minor Skills
-        .text-subtitle2 {{listToText(Resume.skills.minor)}}
-      div
-        .text-subtitle1.text-primary.smCmdL Languages & Frameworks
-        .text-subtitle2 {{listToText(Resume.skills.frameworks)}}
-      div
-        .text-subtitle1.text-primary.smCmdL Software
-        .text-subtitle2 {{listToText(Resume.skills.software)}}
-      div
-        .text-subtitle1.text-primary.smCmdL Education
-        div(v-for="edu in Resume.education")
-          .text-subtitle1 {{edu.degree}} - #[small.text-subtitle2 {{edu.locale}}]
-        .text-subtitle1.text-primary.smCmdL Awards
-        div(v-for="award in Resume.awards")
-          .text-subtitle1 {{award.name}} - #[small {{award.by}}]
+
+      .column.q-gutter-y-lg.q-mb-sm
+        div
+          .text-body1.text-primary.smCmdL Major Skills
+          .text-subtitle2 {{listToText(Resume.skills.major)}}
+
+        div
+          .text-body1.text-primary.smCmdL Minor Skills
+          .text-subtitle2 {{listToText(Resume.skills.minor)}}
+
+        div
+          .text-body1.text-primary.smCmdL Languages & Frameworks
+          .text-subtitle2 {{listToText(Resume.skills.frameworks)}}
+
+        div
+          .text-body1.text-primary.smCmdL Software
+          .text-subtitle2 {{listToText(Resume.skills.software)}}
+
+        div
+          .text-body1.text-primary.smCmdL Education
+          .column.q-gutter-y-sm
+            div(v-for="edu in Resume.education")
+              .text-subtitle1 {{edu.degree}} - #[small.text-subtitle2 {{edu.locale}}]
+
+        div
+          .text-subtitle1.text-primary.smCmdL Awards
+          .column.q-gutter-y-sm
+            div(v-for="award in Resume.awards")
+              .text-subtitle1 {{award.name}} - #[small {{award.by}}]
 
     .col-12.col-md-8.col-lg-9
-      div.container.gt-sm
+      div.container.gt-sm.q-mt-lg.q-mb-xl
         .text-h5.text-primary.text-center Current Work Objective
-        div.text-justify {{Resume.objective}}
+        .text-body1.text-justify {{Resume.objective}}
 
       .text-h5.text-primary.q-mb-md.text-center Work Experience
       .row.q-col-gutter-xs
-        .col-12.col-md-6.col-lg-4(v-for="work in Resume.work")
-          q-card.no-shadow
+        .col-12.col-md-6.col-lg-4(v-for="work in WorkExperience")
+          q-card.no-shadow.q-card-experience
             q-card-section.q-pb-none
               .text-center
-                .text-h6.text-primary {{work.position}}
-                .text-subtitle2 {{work.employer}} - {{work.duration}}
-            q-card-section.q-pt-sm
-              .text-center.text-body1 Responsibilities
-              ul.q-pl-sm.q-mt-xs.text-justify #[li.text-subtitle2(v-for="resp in work.responsibilities") {{resp}}]
+                .text-h6.text-primary {{work.position}} #[span.text-white.text-subtitle2 at {{work.employer}}]
+            q-card-section.q-pt-none
+              .text-center.text-body2 Responsibilities
+              ul.q-pl-sm.q-mt-xs.q-mb-none.text-justify.q-gutter-y-xs
+                li.text-subtitle2(v-for="resp in work.responsibilities") {{resp}}
 </template>
 <script lang="ts">
-import * as resume from 'src/data/resume';
-import { Options, Vue } from 'vue-class-component';
+import resume from "src/data/resume";
+import { Options, Vue } from "vue-class-component";
 
 @Options({})
 export default class Resume extends Vue {
-  Resume = resume.default;
+  Resume = resume;
 
-  listToText(arr: Array<string>){
-    return arr.toString().replace(/,/g, " | ")
+  listToText(arr: Array<string>) {
+    return arr.toString().replace(/,/g, " | ");
+  }
+
+  get WorkExperience(){
+    return resume.work.splice(0,6);
   }
 }
 </script>
@@ -74,6 +87,7 @@ export default class Resume extends Vue {
 
 .space
     flex-grow: 1
+
 .rc
   display: flex
   flex-direction: column
@@ -89,6 +103,10 @@ export default class Resume extends Vue {
 
 .q-card
   min-height: 365px
+
+.q-card-experience
+  min-height: 325px
+
 
 .text-subtitle2
   font-weight: unset
